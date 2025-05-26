f7b0f7376d49b3970bc15a30944ced967a93e6da
vladimir <vladimir.obmolotkov@gmail.com>
2025-05-26 11:10:50 +0300

    Fix CPU and RAM metrics collection in cgroups v2
    In cgroups v2 some fields were removed, which changed the Docker API.
    
    Key changes:
    - Added cgroups v2 compatibility
    - Minor code refactoring
    
    Change-Id: I9190d9aedcae9bd6aea836d8cc49b2fb1fe0506f
    Signed-off-by: vladimir <vladimir.obmolotkov@gmail.com>

--- src/nodemgr/common/docker_mem_cpu.py
+++ src/nodemgr/common/docker_mem_cpu.py
@@ -2,7 +2,6 @@
 # Copyright (c) 2016 Juniper Networks, Inc. All rights reserved.
 #
 
-import os
 import docker
 
 from nodemgr.common.sandesh.nodeinfo.cpuinfo.ttypes import ProcessCpuInfo
@@ -20,44 +19,59 @@ class DockerMemCpuUsageData(object):
     def _get_container_stats(self):
         return self.client.stats(self._id, stream=False)
 
-    def _get_process_cpu_share(self, current_cpu):
-        # sometimes docker returns empty arrays
-        if "cpu_usage" not in current_cpu or "percpu_usage" not in current_cpu["cpu_usage"]:
-            return 0
+    def _get_process_mem_usage(self, stats):
+        memory_stats = stats.get('memory_stats', dict())
+        return memory_stats.get('usage', 0) // 1024
 
-        last_cpu = self.last_cpu
-        last_time = self.last_time
-        current_time = os.times()[4]
-        cpu_count = len(current_cpu["cpu_usage"]["percpu_usage"])
+    def _get_process_mem_resident(self, stats):
+        memory_stats = stats.get('memory_stats', dict())
 
-        # docker returns current/previous cpu stats in call
-        # but it previous data can't be used cause we don't know who calls
-        # stat previously
-        interval_time = 0
-        if last_cpu and (last_time != 0):
-            sys_time = float(current_cpu['cpu_usage']['usage_in_kernelmode'] -
-                             last_cpu['cpu_usage']['usage_in_kernelmode']) / 1e9
-            usr_time = float(current_cpu['cpu_usage']['usage_in_usermode'] -
-                             last_cpu['cpu_usage']['usage_in_usermode']) / 1e9
-            interval_time = current_time - last_time
+        # In the new versions of the kernel, 'cgroups v2' is used, which may not have the 'rss' field
+        if 'rss' in memory_stats.get('stats', dict()):
+            return memory_stats['stats']['rss'] // 1024
 
-        self.last_cpu = current_cpu
-        self.last_time = current_time
+        # Instead of it, we can use the 'anon' field if 'cgroups v2' is used.
+        return memory_stats.get('stats', dict()).get('anon', 0) // 1024
 
-        if interval_time > 0:
-            sys_percent = 100 * sys_time // interval_time
-            usr_percent = 100 * usr_time // interval_time
-            cpu_share = round((sys_percent + usr_percent) / cpu_count, 2)
-            return cpu_share
+    def _get_process_cpu_share(self, stats):
+        cpu_stats = stats.get('cpu_stats', dict())
+        cpu_usage = cpu_stats.get('cpu_usage', dict())
 
-        return 0
+        # In cgroups v2, 'percpu_usage' may be unavailable
+        if 'percpu_usage' in cpu_usage:
+            cpu_count = len(cpu_usage['percpu_usage'])
+        else:
+            cpu_count = cpu_stats.get('online_cpus', 0)
+
+        if cpu_count == 0:
+            return 0.0
+
+        cpu_total = float(cpu_usage['total_usage'])
+        cpu_system = float(cpu_stats['system_cpu_usage'])
+
+        if self.last_cpu and self.last_time:
+            cpu_delta = cpu_total - self.last_cpu
+            system_cpu_delta = cpu_system - self.last_time
+        else:
+            self.last_cpu = cpu_total
+            self.last_time = cpu_system
+            return 0.0
+
+        if system_cpu_delta <= 0.0:
+            return 0.0
+
+        self.last_cpu = cpu_total
+        self.last_time = cpu_system
+
+        cpu_share = (cpu_delta / system_cpu_delta) * cpu_count
+        return round(cpu_share, 2)
 
     def get_process_mem_cpu_info(self):
         stats = self._get_container_stats()
-        cpu_stats = stats['cpu_stats']
-        mem_stats = stats['memory_stats']
         process_mem_cpu = ProcessCpuInfo()
-        process_mem_cpu.cpu_share = self._get_process_cpu_share(cpu_stats)
-        process_mem_cpu.mem_virt = mem_stats.get('usage', 0) // 1024
-        process_mem_cpu.mem_res = mem_stats.get('stats', dict()).get('rss', 0) // 1024
+
+        process_mem_cpu.cpu_share = self._get_process_cpu_share(stats)
+        process_mem_cpu.mem_virt = self._get_process_mem_usage(stats)
+        process_mem_cpu.mem_res = self._get_process_mem_resident(stats)
+
         return process_mem_cpu
